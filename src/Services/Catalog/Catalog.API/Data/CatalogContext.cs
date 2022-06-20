1d73932aa681062d276af1c67ed5ff17d7481123
Jeffrey Onochie <jeff.ict@gmail.com>
2022-06-20 23:33:39 +0100

    add Catalog entity, DB Connection to MongoDB, seed data if none exist for Products collection

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName")); //create DB if it doesn't find one with the name
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        }
+        public IMongoCollection<Product> Products {get;}
+    }
+}
