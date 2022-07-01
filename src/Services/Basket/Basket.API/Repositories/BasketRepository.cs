c1bc287f11d8484408f31077e524424a6f3d2b0a
Jeffrey Onochie <jeff.ict@gmail.com>
2022-07-01 15:16:59 +0100

    implement repository pattern for Basket/Cart

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository: IBasketRepository
+    {
+        /**Haven installed our Microsoft.Extensions.Caching.StackExchangeRedis importing IDistributedCache will integrate StackExcahngeRedis
+         * with Distributed caching of ASP.NET and this is one of the best implementation when working with redis or any kind of distributed
+         * cache**/
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            //basket as returned from redis will be a stringified object, we use JsonConvert.DeserializeObject from d Newtonsoft package
+            //to convert the string to JSON
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
