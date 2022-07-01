ee0a53132f7979cdf52fb2dbedbea7dcd9748308
Jeffrey Onochie <jeff.ict@gmail.com>
2022-07-01 16:50:00 +0100

    add BasketController actions

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,45 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+
+            /**Here ware saying if basket is null, then the user may be a newly created & this may be the 1st item thet want to add to
+             * basket, so we create a new cart for the user**/
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
