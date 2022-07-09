2204a73707c6e3969a0c87eb52c8cbf8f9785b21
Jeffrey Onochie <jeff.ict@gmail.com>
2022-07-09 16:59:34 +0100

    delete sample proto classes

--- src/Services/Discount/Discount.gRPC/Program.cs
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -1,6 +1,6 @@
 using Discount.gRPC.Extensions;
 using Discount.gRPC.Repositories;
-using Discount.gRPC.Services;
+//using Discount.gRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +15,7 @@ var app = builder.Build();
 app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
