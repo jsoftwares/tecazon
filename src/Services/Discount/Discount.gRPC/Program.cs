722109589dc16a7f2b4f91bd6cdb0b697bd23045
Jeffrey Onochie <jeff.ict@gmail.com>
2022-07-10 08:14:11 +0100

    implement Discount service gRPC methods

--- src/Services/Discount/Discount.gRPC/Program.cs
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -1,6 +1,6 @@
 using Discount.gRPC.Extensions;
 using Discount.gRPC.Repositories;
-//using Discount.gRPC.Services;
+using Discount.gRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Discount.gRPC.Entities;
+using Discount.gRPC.Protos;
+using Discount.gRPC.Repositories;
+using Grpc.Core;
+
+namespace Discount.gRPC.Services
+{
+    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository repository, IMapper mapper, ILogger<DiscountService> logger)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repository.GetDiscount(request.ProductName);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with Product Name {request.ProductName} was not found."));
+            }
+
+            _logger.LogInformation("Discount is retrieved for Product Name: {productName}, Amount: {amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            //we would recieve CouponModel here as in Protobuf file but what we store in Postgres is a Coupon entity type
+            //So we use AutoMapper to conver the type
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+            await _repository.CreateDiscount(coupon);
+
+            _logger.LogInformation("Discount is successfully created. Product Name: {productName}", coupon.ProductName);
+
+            //response of CreateDiscount gRPC method still expects CouponModel hence we remart from Coupon to CouponModel
+            //bcos after creating d coupon, d Id is generated, so this time we're also returing d ID info
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+            await _repository.UpdateDiscount(coupon);
+
+            _logger.LogInformation("Discount is successfully updated. Product Name: {productName}", coupon.ProductName);
+
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var deleted = await _repository.DeleteDiscount(request.ProductName);
+            var response = new DeleteDiscountResponse
+            {
+                Success = deleted
+            };
+
+            return response;
+        }
+    }
+}
