13734a3af8a8810fcdcb1f76c3118130de111590
Jeffrey Onochie <jeff.ict@gmail.com>
2022-09-03 16:39:12 +0100

    Setup Ordering service with diff layers using clean architecture & CQRS implementation. Then Develop Ordering.Domain Layer in Clean Architecture

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        //Class provides common fields for the Entities; ie for every entity Ordering microservices we should expect 
+        //the below colums as a default
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+
+    }
+}
