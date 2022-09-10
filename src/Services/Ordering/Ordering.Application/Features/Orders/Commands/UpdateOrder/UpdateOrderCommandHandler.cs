d878c180e389d81c6a1b59f627f48518119a43cb
Jeffrey Onochie <jeff.ict@gmail.com>
2022-09-10 13:59:47 +0100

    Developing Ordering.Application Layer- Application Command Features for UpdateOrder

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    /**If we are not returning any object as response from our command or any type of request, Mediator expects that 
+     * we return a Unit hence our handle mwthod returning a <Unit> type **/
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ILogger<UpdateOrderCommand> _logger;
+        private readonly IMapper _mapper;
+
+        public UpdateOrderCommandHandler(IOrderRepository orderRepository, ILogger<UpdateOrderCommand> logger, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            //Biz requirement: update order
+            var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
+            if (orderToUpdate == null)
+            {
+                _logger.LogError("Order was not found in database.");
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            //_mapper.Map(source, destination, sourceType, destinationType)
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _orderRepository.UpdateAsync(orderToUpdate);
+            _logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated.");
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{Username} is required")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{Username} must not exceed 50 characters");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required")
+                .GreaterThan(0).WithMessage("{TotalPrice} must not greater than 0");
+        }
+    }
+}
