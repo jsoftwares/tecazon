ec5cacb775d09805accdf8a5e984748e80836c9f
Jeffrey Onochie <jeff.ict@gmail.com>
2022-09-23 07:51:49 +0100

    Developing Ordering.API Presentation Layer in Clean Architecture. CRUD operations

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,69 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrderList;
+using System.Net;
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        /**For all operations here we need the Mediator object inserted into the ASP.Netcore dependency injection.
+         * You can use a package installed in a referenced project without having to install it again in the referencing project
+         * eg we had installed MediaR package in Ordering Application and and we used it here without installing again. You may just
+         * need to build the referenced project to have it reflect here. **/
+
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{userName}", Name = "GetOrders")]
+        [ProducesResponseType(typeof(IEnumerable<OrdersVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrdersVm>>> GetOrdersByUserName(string userName)
+        {
+            var query = new GetOrderListQuery(userName);
+            var orders = await _mediator.Send(query);
+
+            return Ok(orders);
+        }
+
+        //testing CQRS Order Design pattern implementation use only as checkout will be triggered from RabbbitMQ, hence method will not be used
+        /**We are throwing diff types of status code bcos in our mediator UpdateOrderCommandHandler we're throwing exceptions: if d Order
+         * is not found we threw a notfound exception there and we return nothing after updating, so we are catering for those here**/
+        [HttpPost(Name = "CheckoutOrder")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut(Name = "UpdateOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteOrder")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            var command = new DeleteOrderCommand() { Id = id};
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
