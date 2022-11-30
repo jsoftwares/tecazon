51ab3960935885a13920e4e4fd0767fba480f0f6
Jeffrey Onochie <jeff.ict@gmail.com>
2022-11-30 02:12:08 +0100

    Add Ordering.Infrastructure Layer in Clean Architecture - Repositories & Email

---
+++ src/Services/Ordering/Ordering.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Ordering.Infrastructure.Email
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; }
+        public ILogger<EmailService> _logger;
+
+        //Since we are getting EmailSettings from application setting, it should be IOption(using Microsoft.Extensions); this 
+        //will help us get application settings in a structured way
+        public EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger)
+        {
+            _emailSettings = emailSettings.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> SendEMail(Application.Models.Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            //var from = new EmailAddress("hello@cbtexams.com", "Tecazon");
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _ - _emailSettings.FromName
+            };
+
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+
+            _logger.LogInformation("Email sent!");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+                return true;
+
+            _logger.LogError("Email sending falled");
+            return false;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    /**Now we create d OrderRepository which inherits from RepositoryBase; this is for d Order common operations. IOrderRepository
+     * interface is to implement d custom queries specific for Order. Since this class inherits from RepositoryBase, we inherit all
+     * the public&protected methods there hence we are able use all the basic CRUD methods in RepositoryBase. **/
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        /**We're adding this contructor here bcos when you have a base class as a constructor with a parameter, you have to add this
+         * constructor parameter method with the sub classes; this is basic OOP principles**/
+        public OrderRepository(OrderContext dbCcontext) : base(dbCcontext)
+        {
+
+        }
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                .Where(o => o.UserName == userName)
+                .ToListAsync();
+
+            return orderList;
+        }
+    }
+}
