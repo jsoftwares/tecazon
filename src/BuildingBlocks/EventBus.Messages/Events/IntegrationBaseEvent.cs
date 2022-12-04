1c2a92d537b93c6856e414919f9dcf60bcbf10d6
Jeffrey Onochie <jeff.ict@gmail.com>
2022-12-04 05:40:03 +0100

    Add BuildingBlocks EventBus.Messages Class Library

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**Cross-cutting-concerns refers to classes/code that is share between multiple services - Basket & Order microservices are are 
+ * using RabbitMQ to dispatch & consumes order requests respectively, so we use cross-cutting-concerns here. We create the new
+ * Solution Folder under Solutions (same level as Services), then we add a new folder (BuildingBlocks) then add a new project 
+ * (class library) in /src/BuildingBlocks
+ * This DTO will be common between BasketCheckout event and CreateOrder event**/
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public Guid Id { get; private set; }
+        public DateTime CreateDate { get; private set; }
+
+        //since these properties are d private set; we provide these ctor providing these values with Id & CreateDate
+        //if you don't provide parameters to d ctor it generate a new Id & use current Date/Time for parameters.
+        //If you provide parameter, d 2nd ctor is invoked & your parameters are used
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreateDate = DateTime.UtcNow;
+        }
+        public IntegrationBaseEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreateDate = createDate;
+        }
+    }
+}
