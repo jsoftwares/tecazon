d4cf67a114d162a1575bd9b895cc22d823cccf36
Jeffrey Onochie <jeff.ict@gmail.com>
2022-12-13 13:09:53 +0100

    Add Dto Model Class for Api Aggreation Operations

--- src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
@@ -1,14 +1,8 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace Catalog.API.Entities
+﻿namespace Shopping.Aggregator.Models
 {
-    public class Product
+    public class CatalogModel
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        [BsonElement("Name")]
         public string Name { get; set; }
         public string Category { get; set; }
         public string Summary { get; set; }
---
+++ src/ApiGateways/Shopping.Aggregator/Models/ShoppingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    /**We use this for the response we are to send to client applications when they send a request with a given username**/
+    public class ShoppingModel
+    {
+        public string UserName { get; set; }
+        public BasketModel BasketWithProducts { get; set; }
+        public IEnumerable<OrderResponseModel> Orders { get; set; }
+    }
+}
