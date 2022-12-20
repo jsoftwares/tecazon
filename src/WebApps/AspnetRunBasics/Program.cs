a2844d0f7c1e15ef0055b4d23ea09189e9219d01
Jeffrey Onochie <jeff.ict@gmail.com>
2022-12-20 11:38:20 +0100

    Revert Program.cs to default, refactoring Developments in AspnetBasics Shopping Web Application Microservices

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,9 +1,5 @@
-using System;
-using AspnetRunBasics.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace AspnetRunBasics
 {
@@ -11,9 +7,7 @@ namespace AspnetRunBasics
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            SeedDatabase(host);
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -23,24 +17,5 @@ namespace AspnetRunBasics
                     webBuilder.UseStartup<Startup>();
                 });
 
-        private static void SeedDatabase(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-
-                try
-                {
-                    var aspnetRunContext = services.GetRequiredService<AspnetRunContext>();
-                    AspnetRunContextSeed.SeedAsync(aspnetRunContext, loggerFactory).Wait();
-                }
-                catch (Exception exception)
-                {
-                    var logger = loggerFactory.CreateLogger<Program>();
-                    logger.LogError(exception, "An error occurred seeding the DB.");
-                }
-            }
-        }
     }
 }
