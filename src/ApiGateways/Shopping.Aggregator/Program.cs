fdc4c3e467ad132ca9966d4733729b97879e29fe
Jeffrey Onochie <jeff.ict@gmail.com>
2023-05-27 17:55:57 +0100

    Apply Retry Pattern with Polly policies on HttpClientFactory for Shopping.Aggregator

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,4 +1,5 @@
 using Common.Logging;
+using Polly;
 using Serilog;
 using Shopping.Aggregator.Services;
 
@@ -8,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog(SeriLogger.Configure);
 
 builder.Services.AddTransient<LoggingDelegatingHandler>();
-
+/**We modify our HttpClient factory implementation in other to introduce Resillience & Fault Handling with Http client. For this purpose we use Polly with IHttpClientFactory.
+ * Polly has an extension (Microsoft.Extensions.Http.Polly for working with HTTP client & we will use that since we are using IHttpClientFactory. In Basket, we retry 
+ * after 2 seconds for 3 times **/
 // Add services to the container.
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]))
@@ -16,7 +19,8 @@ builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
 
 builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]))
-    .AddHttpMessageHandler<LoggingDelegatingHandler>();
+    .AddHttpMessageHandler<LoggingDelegatingHandler>()
+    .AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(2)));
 
 builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]))
